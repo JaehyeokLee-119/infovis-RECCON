9b43df98e107c44cda5e39f5ff91365e9e12a2a2
Lee Jae Hyeok <hjl8708@skku.edu>
2023-05-29 02:04:40 +0900

    :hammer: 'b' 관련 디버깅(network)

--- components/network.js
+++ components/network.js
@@ -1,15 +1,20 @@
 class Network {
     margin = {
-        top: 50, right: 50, bottom: 10, left: 50
+        top: 10, right: 10, bottom: 0, left: 10
     }
 
     constructor(svg1, svg2, data, width = 600, height = 250) {
-        this.svg_graph = svg1;
+        this.svg_legend = svg1;
         this.svg_matrix = svg2;
         this.data = data;
-        this.width = width;
-        this.height = height;
+        const svg_element = d3.select(this.svg_matrix);
+        this.width = svg_element.node().getBoundingClientRect().width;
+        this.height = svg_element.node().getBoundingClientRect().height;
+        console.log(this.width, this.height)
+        
+        this.fontsize = 13;
         this.emotion_color_policy = { // From d3.schemeCategory10
+            'X': '#FFFFFF', // white
             'happy': '#ff7f0e',
             'neutral': '#7f7f7f',
             'angry': '#d62728',
@@ -20,6 +25,9 @@ class Network {
             'excited': '#17becf'
         };
         this.emotion_cause_pair_list = [];
+
+        this.matrix_box_width = width/9;
+
     }
 
     initialize() {
@@ -28,12 +36,59 @@ class Network {
             'emotion': [],
             'cause': [] // 한 쌍은 같은 index에 저장됨
         };
+
+        let emotion_list = ['happy', 'neutral', 'angry', 'surprise', 'disgust', 'sad', 'fear', 'excited'];
+        let legend_box = d3.selectAll(this.svg_legend)
+            .attr('transform', `translate(${this.width/3}, ${35})`)   
+            .selectAll('*').remove()
+
+        // emotion list에 있는 색깔로 legend를 만든다.
+        let second_line_x_offset = 10*this.fontsize;
+        for(var i = 0; i < emotion_list.length; i++) {
+            let emotion = emotion_list[i];
+            let color = this.emotion_color_policy[emotion];
+            let legend = d3.select(this.svg_legend).append('g');
+            legend.append('rect')
+                .attr('x', () => {
+                    if (i>=4) return second_line_x_offset;
+                    else return 0;
+                })
+                // .attr('y', i * this.fontsize * 1.5)
+                .attr('y', () => {
+                    if (i>=4) return (i-4) * this.fontsize * 1.5;
+                    else return i * this.fontsize * 1.5;
+                })
+                .attr('dx', () => {
+                    if (i>=4) return second_line_x_offset;
+                    else return 0;
+                })
+                .attr('width', this.fontsize)
+                .attr('height', this.fontsize)
+                .attr('fill', color);
+            legend.append('text')
+                .attr('x', this.fontsize * 1.5)
+                .attr('dx', () => {
+                    if (i>=4) return second_line_x_offset;
+                    else return 0;
+                })
+                // .attr('y', i * this.fontsize * 1.5 + this.fontsize)
+                .attr('y', () => {
+                    if (i>=4) return (i-4) * this.fontsize * 1.5 + this.fontsize;
+                    else return i * this.fontsize * 1.5 + this.fontsize;
+                })
+                .attr('font-size', this.fontsize)
+                .text(emotion);
+        }
+
     }
     update(did_list) {
         this.utterance_list = [];
+        this.emotion_cause_pair_list = {
+            'emotion': [],
+            'cause': [] // 한 쌍은 같은 index에 저장됨
+        };
         // did_list는 Emotion-cause link adjacency matrix를 계산하기 위한 dialog_id list
         
-        did_list = ['tr_4466', 'tr_183'];
         let dialog_start_index_list = []; // utterance_list에서 각 dialog의 시작 index를 저장
         // Get utterances with dialog_id
 
@@ -50,6 +105,7 @@ class Network {
                 current_start_index++;
             }
         }
+        console.log(this.utterance_list);
 
         for(var i = 0; i < dialog_start_index_list.length; i++) {
             let start_index = dialog_start_index_list[i];
@@ -61,12 +117,17 @@ class Network {
             for(var j = start_index; j < end_index; j++) { // j: this.utterance_list 속에서 현재 발화의 index
                 let current_utterance = this.utterance_list[j];
                 if(current_utterance.emotion != 'neutral') {
-
                     // current_utterance.cause_turn에서 [와 ]를 제거하고 배열로 만듦
                     current_utterance.cause_turn = current_utterance.cause_turn.replaceAll('[', '');
                     current_utterance.cause_turn = current_utterance.cause_turn.replaceAll(']', '');
                     current_utterance.cause_turn = current_utterance.cause_turn.replaceAll(' ', '');
                     let cause_turns = current_utterance.cause_turn.split(',');
+
+                    // 'b' 제거 전
+                    // cause_turns에서 숫자인 원소만 남기기
+                    cause_turns = cause_turns.filter(x => !isNaN(x));
+                    // 'b' 제거 후
+                    if (cause_turns.length == 0) continue;
                     cause_turns = cause_turns.map(x => parseInt(x) - 1); // cause_turns에 1을 뺀다 (turn 수 -> index)
 
                     // cause_turns: current_utterance의 cause 발화의 index 배열
@@ -100,5 +161,178 @@ class Network {
             emotion_cause_link_adjacency_matrix.push(current_row);
         }
         console.log('emotion_cause_link_adjacency_matrix: ', emotion_cause_link_adjacency_matrix);
+
+
+        d3.selectAll(this.svg_matrix).selectAll('*').remove();
+        
+        // Adjacency_matrix
+        // -> emotion_cause_link_adjacency_matrix를 이용하여, emotion_cause_link_adjacency_matrix를 그린다
+        let matrix_width = this.width - this.margin.left - this.margin.right - this.fontsize;
+        let matrix_height = this.height - this.margin.top - this.margin.bottom;
+        let matrix_box_width = matrix_width / 9;
+        let matrix_box_height = matrix_height / 9;
+
+        // matrix는 좌측은 emotion, 상단은 cause로 구성된다.
+        // label은 글자(emotion_list)가 아니라, 각 emotion에 해당하는 color(this.emotion_color_policy)로 채워진다.
+
+        // 전체를 조금 오른쪽아래로 옮기기
+        d3.select(this.svg_matrix) 
+            .attr('dx', 10)
+            .attr('dy', 10)
+            .attr('transform', `translate(${this.margin.left+this.fontsize}, ${this.margin.top+this.fontsize})`)
+
+        let matrix = d3.select(this.svg_matrix)
+            .attr('width', matrix_width+10)
+            .attr('height', matrix_height+10)
+            .append('g')
+            .attr('transform', `translate(${this.margin.left+this.fontsize}, ${this.margin.top+this.fontsize})`)
+
+        // 첫번째 행: emotion_list ( 9개의 박스, 첫번째 1개는 빈 박스, 나머지 8개는 emotion_list의 각 emotion에 대응되는 색깔로 채워진 박스)
+        
+        
+        // rect를 만들어서 background color를 emotion 색깔로 한다
+        let emotion_list_with_first_X = ['X'].concat(emotion_list);
+        matrix.append('g')
+            .selectAll('rect')
+            .data(emotion_list_with_first_X)
+            .enter()
+            .append('rect')
+            .attr('x', (d, i) => {
+                if (i == 0) {
+                    return 0;
+                } else {
+                    return i * matrix_box_width - matrix_box_width/2;
+                }
+            })
+            .attr('y', 0)
+            .attr('width', (d, i) => {
+                if (i == 0) {
+                    return matrix_box_width/2;
+                }
+                else {
+                    return matrix_box_width;
+                }
+            })
+            .attr('height', matrix_box_height/2)
+            .attr('fill', d => this.emotion_color_policy[d])
+            .attr('stroke', 'black')
+            .attr('stroke-width', 1)
+            .attr('stroke-opacity', 0.5)
+            .attr('fill-opacity', 1)
+            .attr('class', 'emotion_list_rect')
+            .attr('id', (d, i) => `emotion_list_rect_${i}`)
+            
+        // 그 아래 행들: emotion_cause_link_adjacency_matrix
+        // 각 행의 첫번째 박스는 emotion_list의 각 emotion에 해당하는 색깔로 채워진다.
+        
+        // 세로 범례 그리기 (똑같은 걸 세로로 그린다)
+        matrix.append('g')
+            .selectAll('rect')
+            .data(emotion_list)
+            .enter()
+            .append('rect')
+            .attr('x', 0)
+            .attr('y', (d, i) => {
+                return i * matrix_box_height + matrix_box_height/2;
+            })
+            .attr('width', matrix_box_width/2)
+            .attr('height', matrix_box_height)
+            .attr('fill', d => this.emotion_color_policy[d])
+            .attr('stroke', 'black')
+            .attr('stroke-width', 1)
+            .attr('stroke-opacity', 0.5)
+            .attr('fill-opacity', 1)
+            .attr('class', 'emotion_list_rect')
+
+        // 시작 x는 matrix_box_width/2, 시작 y는 matrix_box_height/2
+        // emotion_cause_link_adjacency_matrix를 갖고, 각 칸의 크기가 (matrix_box_width, matrix_box_height)인 matrix를 적는다(text)
+        
+        for(var row = 0; row < emotion_cause_link_adjacency_matrix.length; row++) {
+            matrix.append('g')
+                .selectAll('text')
+                .data(emotion_cause_link_adjacency_matrix[row])
+                .enter()
+                .append('text')
+                .attr('x', (d, i) => {
+                    return i * matrix_box_width + matrix_box_width/2;
+                })
+                .attr('y', (d, i) => {
+                    return row * matrix_box_height + matrix_box_height/2;
+                })
+                .attr('dx', matrix_box_width/2)
+                .attr('dy', matrix_box_height/2)
+                .attr('text-anchor', 'middle')
+                .attr('dominant-baseline', 'middle')
+                .text(d => d)
+                .attr('class', 'matrix_text')
+                .attr('id', (d, i) => `matrix_text_${row}_${i}`)
+                .attr('font-size', this.fontsize)
+            }
+
+        // adjacency_matrix에 가로세로줄 긋기
+        
+        // 가로줄
+        let end_x = emotion_list_with_first_X.length * matrix_box_width - matrix_box_width/2;
+        let end_y = emotion_list_with_first_X.length * matrix_box_height - matrix_box_height/2;
+        matrix.append('g')
+            .selectAll('line')
+            .data(emotion_list_with_first_X)
+            .enter()
+            .append('line')
+            .attr('x1', 0)
+            .attr('y1', (d, i) => {
+                return i * matrix_box_height + matrix_box_height/2;
+            })
+            .attr('x2', end_x)
+            .attr('y2', (d, i) => {
+                return i * matrix_box_height + matrix_box_height/2;
+            })
+            .attr('stroke', 'black')
+            .attr('stroke-width', 1)
+            .attr('stroke-opacity', 0.5)
+            .attr('class', 'matrix_line')
+        
+        // 세로줄
+        matrix.append('g')
+            .selectAll('line')
+            .data(emotion_list_with_first_X)
+            .enter()
+            .append('line')
+            .attr('x1', (d, i) => {
+                return i * matrix_box_width + matrix_box_width/2;
+            })
+            .attr('y1', 0)
+            .attr('x2', (d, i) => {
+                return i * matrix_box_width + matrix_box_width/2;
+            })
+            .attr('y2', end_y)
+            .attr('stroke', 'black')
+            .attr('stroke-width', 1)
+            .attr('stroke-opacity', 0.5)
+            .attr('class', 'matrix_line')
+
+        // 축 이름 넣기 (가로축: Cause utterance emotion, 세로축: Emotion utterance emotion (90도 돌려서))
+        // 가로축
+        d3.select(this.svg_matrix)
+            .append('text')
+            .attr('x', end_x/2)
+            .attr('y', 0)
+            .attr('dx', matrix_box_width/2)
+            .text('Cause utterance emotion')
+            .attr('text-anchor', 'middle')
+            .attr('dominant-baseline', 'hanging')
+            .attr('font-size', this.fontsize)
+        
+        // 세로축
+        d3.select(this.svg_matrix)
+            .append('text')
+            .attr('x', 0)
+            .attr('y', end_y/2)
+            .text('Emotion utterance emotion')
+            .attr('text-anchor', 'middle')
+            .attr('dominant-baseline', 'baseline')
+            .attr('font-size', this.fontsize) 
+            .attr('transform', `rotate(90, 0, ${end_y/2})`)
+
     }
 }
--- components/overview.js
+++ components/overview.js
@@ -56,88 +56,80 @@ class Overview {
         }
         // this.container.call(this.brush);
 
-        // svg의 정 중앙에 가로줄을 긋는다
-        d3.select('svg')
-            .append("line")
-            .attr("x1", 0)
-            .attr("y1", this.height / 2)
-            .attr("x2", this.width)
-            .attr("y2", this.height / 2)
-            .attr("stroke", "black")
-            .attr("stroke-width", 0.3)
-
         // 이제 여기다가 각 데이터를 길이(len) 순으로 정렬하고, 그 길이에 맞게 세로줄로 나타내어 가로로 나열한다.
 
-        data.sort(function(a, b) {
-            return a.len - b.len;
-        })
+        // // 정렬 할까 말까
+        // data.sort(function(a, b) {
+        //     return a.len - b.len;
+        // })
+
+        let mouseover_event = (e, d) => {
+            // 1. 툴팁표시
+            this.tooltip.select(".tooltip-inner")
+                .html(`ID: ${d.dialog_id}<br />length: ${d.len}`);
+
+            Popper.createPopper(e.target, this.tooltip.node(), {
+                placement: 'top',
+                modifiers: [
+                    {
+                        name: 'arrow',
+                        options: {
+                            element: this.tooltip.select(".tooltip-arrow").node(),
+                        },
+                    },
+                ],
+            });
+            this.tooltip.style("display", "block");
+            
+            let tooltip_arrow_style = this.tooltip.select(".tooltip-arrow").attr("style");
+            let tooltip_arrow_transform_x = tooltip_arrow_style.split("translate(")[1].split("px")[0];
+            let tooltip_inner_width = this.tooltip.select(".tooltip-inner").node().getBoundingClientRect().width;
+            this.tooltip.select(".tooltip-inner")
+                .attr("style", `transform: translate(${tooltip_arrow_transform_x-tooltip_inner_width/2}px, 0px)`)
+            
+            // 2. e 대상(마우스 올라간 대상)의 line의 stroke를 두껍게 하고 보여지는 세로 크기를 늘린다.
+            e.target.setAttribute("stroke-width", this.line_hover_stroke);
+            e.target.setAttribute("y1", ((this.height / 2) + parseInt(d.len)*2) + this.line_hover_added_length);
+            e.target.setAttribute("y2", ((this.height / 2) - parseInt(d.len)*2) - this.line_hover_added_length);
+        }
+        let mouseout_event = (e, d) => {
+            this.tooltip.style("display", "none");
+            // 2. line의 stroke를 두꺼워졌던 걸 원래대로
+            e.target.setAttribute("stroke-width", this.line_initial_stroke);
+            e.target.setAttribute("y1", ((this.height / 2) + parseInt(d.len)*2));
+            e.target.setAttribute("y2", ((this.height / 2) - parseInt(d.len)*2));
+
+            if (e.target.getAttribute("clicked") == "true") {
+                e.target.setAttribute("stroke-width", this.line_hover_stroke);
+                e.target.setAttribute("y1", ((this.height / 2) + parseInt(d.len)*2) + this.line_hover_added_length);
+                e.target.setAttribute("y2", ((this.height / 2) - parseInt(d.len)*2) - this.line_hover_added_length);
+            }
+
+        }
+        let click_event = (e, d) => {
+            // line 중에 clicked가 true인 것을 찾는다
+            let clicked_line = d3.selectAll("line").filter(function(d, i) {
+                if (this.getAttribute("clicked") == "true") {
+                    this.setAttribute("clicked", "false");
+                    return true;
+                }})
+                .attr("stroke-width", this.line_initial_stroke)
+                .attr("y1", (d, i) => ((this.height / 2) + parseInt(d.len)*2))
+                .attr("y2", (d, i) => ((this.height / 2) - parseInt(d.len)*2));
+            // clicked를 false로 바꾼다
+
+            e.target.setAttribute("clicked", "true");
+
+            this.handlers["click"](d);
+        }
 
         this.lineplots = d3.select('svg')
             .selectAll("line")
             .data(data)
             .join("line")
-            .on("mouseover", (e, d) => {
-                // 1. 툴팁표시
-                this.tooltip.select(".tooltip-inner")
-                    .html(`ID: ${d.dialog_id}<br />length: ${d.len}`);
-
-                Popper.createPopper(e.target, this.tooltip.node(), {
-                    placement: 'top',
-                    modifiers: [
-                        {
-                            name: 'arrow',
-                            options: {
-                                element: this.tooltip.select(".tooltip-arrow").node(),
-                            },
-                        },
-                    ],
-                });
-                this.tooltip.style("display", "block");
-                
-                let tooltip_arrow_style = this.tooltip.select(".tooltip-arrow").attr("style");
-                let tooltip_arrow_transform_x = tooltip_arrow_style.split("translate(")[1].split("px")[0];
-                let tooltip_inner_width = this.tooltip.select(".tooltip-inner").node().getBoundingClientRect().width;
-                this.tooltip.select(".tooltip-inner")
-                    .attr("style", `transform: translate(${tooltip_arrow_transform_x-tooltip_inner_width/2}px, 0px)`)
-                
-                // 2. e 대상(마우스 올라간 대상)의 line의 stroke를 두껍게 하고 보여지는 세로 크기를 늘린다.
-                e.target.setAttribute("stroke-width", this.line_hover_stroke);
-                e.target.setAttribute("y1", ((this.height / 2) + parseInt(d.len)*2) + this.line_hover_added_length);
-                e.target.setAttribute("y2", ((this.height / 2) - parseInt(d.len)*2) - this.line_hover_added_length);
-
-            })
-            .on("mouseout", (e, d) => {
-                this.tooltip.style("display", "none");
-                // 2. line의 stroke를 두꺼워졌던 걸 원래대로
-                e.target.setAttribute("stroke-width", this.line_initial_stroke);
-                e.target.setAttribute("y1", ((this.height / 2) + parseInt(d.len)*2));
-                e.target.setAttribute("y2", ((this.height / 2) - parseInt(d.len)*2));
-
-                if (e.target.getAttribute("clicked") == "true") {
-                    e.target.setAttribute("stroke-width", this.line_hover_stroke);
-                    e.target.setAttribute("y1", ((this.height / 2) + parseInt(d.len)*2) + this.line_hover_added_length);
-                    e.target.setAttribute("y2", ((this.height / 2) - parseInt(d.len)*2) - this.line_hover_added_length);
-                }
-
-            })
-            // mouse click시 index.html에 dialog_id 전달
-            .on("click", (e, d) => {
-                // line 중에 clicked가 true인 것을 찾는다
-                let clicked_line = d3.selectAll("line").filter(function(d, i) {
-                    if (this.getAttribute("clicked") == "true") {
-                        this.setAttribute("clicked", "false");
-                        return true;
-                    }})
-                    .attr("stroke-width", this.line_initial_stroke)
-                    .attr("y1", (d, i) => ((this.height / 2) + parseInt(d.len)*2))
-                    .attr("y2", (d, i) => ((this.height / 2) - parseInt(d.len)*2));
-                // clicked를 false로 바꾼다
-
-                e.target.setAttribute("clicked", "true");
-
-                this.handlers["click"](d);
-            })
-
+            .on("mouseover", mouseover_event)
+            .on("mouseout", mouseout_event)
+            .on("click", click_event)
 
         this.lineplots
             .attr("x1", (d, i) => i)
@@ -148,7 +140,8 @@ class Overview {
             .attr("stroke-width", this.line_initial_stroke)
             .attr("stroke", (d, i) => this.emotion_color_policy[d.emotion])
     }
-
+    // 갑자기 mouse 관련 event 동작 안하게 된 이유는: lineplots를 update할 때, mouseover, mouseout, click event를 다시 등록해주지 않아서 그렇다.
+    // update할 때마다 event를 다시 등록해주어야 한다.
     on(eventType, handler) {
         this.handlers[eventType] = handler;
     }
